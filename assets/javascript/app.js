c73b369ba9d915f4e74759724231f862d9e7d283
Sean McMahon <sean.mcmahon87@gmail.com>
2020-09-22 09:13:06 +0100

    nav text drop animation updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+// NAV TEXT DROP ANIMATION (inspired by youtube tutorial by Dev Ed:https://youtu.be/GUEB9FogoP8)
+const text1 = document.querySelector(".drop1");
+const strText1 = text1.textContent;
+//create split array
+const splitText1 = strText1.split("");
+//remove original text
+text1.textContent = "";
+
+//loop through split array to add split text to individual spans
+for (let i = 0; i < splitText1.length; i++) {
+  text1.innerHTML += "<span>" + splitText1[i] + "</span>";
+}
+
+let char1 = 0;
+let timer1 = setInterval(onTick1, 300);
+
+//add class of fade to all characters at an interval
+function onTick1() {
+  const span1 = text1.querySelectorAll("span")[char1];
+  span1.classList.add("fade");
+  char1++;
+  if (char1 === splitText1.length) {
+    complete1();
+    return;
+  }
+}
+
+//clear timer at end of function
+function complete1() {
+  clearInterval(timer1);
+  timer1 = null;
+}
+
+//SECOND DROP
+const text2 = document.querySelector(".drop2");
+const strText2 = text2.textContent;
+//create split array
+const splitText2 = strText2.split("");
+//remove original text
+text2.textContent = "";
+
+//loop through split array to add split text to individual spans
+for (let i = 0; i < splitText2.length; i++) {
+  text2.innerHTML += "<span>" + splitText2[i] + "</span>";
+}
+
+let char2 = 0;
+let timer2 = setInterval(onTick2, 110);
+
+function onTick2() {
+  const span2 = text2.querySelectorAll("span")[char2];
+  span2.classList.add("fade");
+  char2++;
+  if (char2 === splitText2.length) {
+    complete2();
+    return;
+  }
+}
+
+function complete2() {
+  clearInterval(timer2);
+  timer2 = null;
+}
