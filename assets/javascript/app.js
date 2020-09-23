f62485028968370a6e8d9775f871baa23f93b4ca
Sean McMahon <sean.mcmahon87@gmail.com>
2020-09-23 08:18:12 +0100

    overflow hidden added for nav toggle

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -69,11 +69,7 @@ const burger = document.querySelector(".burger");
 function navToggle(e) {
   if (!e.target.classList.contains("active")) {
     e.target.classList.add("active");
-    gsap.to(".line1", 0.5, {
-      rotate: "45",
-      y: 9.5,
-      background: "var(--bg1)",
-    });
+    gsap.to(".line1", 0.5, { rotate: "45", y: 9.5, background: "var(--bg1)" });
     gsap.to(".line2", 0.5, { autoAlpha: 0 });
     gsap.to(".line3", 0.5, {
       rotate: "-45",
@@ -82,6 +78,7 @@ function navToggle(e) {
     });
     gsap.to(".drop1, .drop2", 1, { color: "var(--bg1)" });
     gsap.to(".toggle-nav", 1, { clipPath: "circle(2500px at 100% -10%" });
+    document.body.classList.add("hide");
   } else {
     e.target.classList.remove("active");
     gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "var(--text1)" });
@@ -89,6 +86,7 @@ function navToggle(e) {
     gsap.to(".line3", 0.5, { rotate: "0", y: 0, background: "var(--text1)" });
     gsap.to(".toggle-nav", 1, { clipPath: "circle(50px at 100% -20%" });
     gsap.to(".drop1, .drop2", 1, { color: "var(--text1)" });
+    document.body.classList.remove("hide");
   }
 }
 
