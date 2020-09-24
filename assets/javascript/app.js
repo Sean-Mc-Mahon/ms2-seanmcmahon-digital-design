53a871ee05fc2360966e70b50c786c88c2c127e8
Sean McMahon <sean.mcmahon87@gmail.com>
2020-09-24 09:07:12 +0100

    dark mode toggle styles, footer styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -107,10 +107,10 @@ var checkbox = document.querySelector("input[name=theme");
 checkbox.addEventListener("change", function () {
   if (this.checked) {
     trans();
-    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("data-theme", "light");
   } else {
     trans();
-    document.documentElement.setAttribute("data-theme", "light");
+    document.documentElement.setAttribute("data-theme", "dark");
   }
 });
 
