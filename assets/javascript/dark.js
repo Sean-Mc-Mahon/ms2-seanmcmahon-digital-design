0f2209866713ba644a0ff2b8ba85f631ef3e3aea
Sean McMahon <sean.mcmahon87@gmail.com>
2020-10-08 14:50:02 +0100

    font sizes altered

--- assets/javascript/dark.js
+++ assets/javascript/dark.js
@@ -7,22 +7,13 @@ let checkbox = document.querySelector("input[name=theme");
 
 checkbox.addEventListener("change", function () {
   if (this.checked) {
-    localStorage.setItem("style", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
   } else {
-    localStorage.setItem("style", "light");
+    document.documentElement.setAttribute("data-theme", "light");
   }
-  applyStyle();
 });
 
-function applyStyle(style = localStorage.getItem("style")) {
-  if (style === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-    document.querySelector("#darkLight").checked = true;
-  } else if (style === "light") {
-    document.documentElement.setAttribute("data-theme", "light");
-    document.querySelector("#darkLight").checked = false;
-  }
-}
+
 
 
 
