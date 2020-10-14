a4a30cf6692ed38a7389b9d052524f0b1ed79666
Sean McMahon <sean.mcmahon87@gmail.com>
2020-10-14 10:48:11 +0100

    page transitions removed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -223,7 +223,7 @@ function initMap() {
   });
 }
 
-//hOME IMAGE GALLERY
+//HOME IMAGE GALLERY
 $("svg#svg-render").hover(function () {
   $("svg#svg-render").addClass("active-svg");
   $("img.img-render").addClass("active-img-link");
@@ -290,3 +290,179 @@ function animateSlides() {
 }
 
 animateSlides();
+
+//Code modified from slack call in #interactive-front-end channel led by Eamon_lead on 23-09-20
+//Grab radio Buttons
+function getUserStyleSetting() {
+  myRadioSett = document.querySelectorAll('input[name="set-style"]');
+
+  let i,
+    max = myRadioSett.length;
+
+  for (i = 0; i < max; i++) {
+    myRadioSett[i].onclick = function () {
+      let style = this.value[0].toUpperCase() + this.value.slice(1);
+      sessionStorage.setItem("chosenStyle", style);
+      applyStyle();
+    };
+  }
+}
+function applyStyle(style = sessionStorage.getItem("chosenStyle")) {
+  if (style === "Blue") {
+    let target = document.querySelectorAll(".color");
+    for (let i = 0; i < target.length; i++) {
+      target[i].classList.remove("normal");
+      target[i].classList.remove("pink");
+      target[i].classList.add("blue");
+      target[i].classList.remove("yellow");
+      target[i].classList.remove("green");
+      target[i].classList.remove("brown");
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice2").checked = false;
+      document.querySelector("#styleChoice3").checked = true;
+      document.querySelector("#styleChoice4").checked = false;
+      document.querySelector("#styleChoice5").checked = false;
+      document.querySelector("#styleChoice6").checked = false;
+    }
+  } else if (style === "Pink") {
+    let target = document.querySelectorAll(".color");
+    for (let i = 0; i < target.length; i++) {
+      target[i].classList.remove("normal");
+      target[i].classList.remove("blue");
+      target[i].classList.add("pink");
+      target[i].classList.remove("yellow");
+      target[i].classList.remove("green");
+      target[i].classList.remove("brown");
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice2").checked = false;
+      document.querySelector("#styleChoice3").checked = false;
+      document.querySelector("#styleChoice4").checked = false;
+      document.querySelector("#styleChoice5").checked = true;
+      document.querySelector("#styleChoice6").checked = false;
+    }
+  } else if (style === "Normal") {
+    let target = document.querySelectorAll(".color");
+    for (let i = 0; i < target.length; i++) {
+      target[i].classList.remove("blue");
+      target[i].classList.remove("pink");
+      target[i].classList.add("normal");
+      target[i].classList.remove("yellow");
+      target[i].classList.remove("green");
+      target[i].classList.remove("brown");
+      document.querySelector("#styleChoice1").checked = true;
+      document.querySelector("#styleChoice2").checked = false;
+      document.querySelector("#styleChoice3").checked = false;
+      document.querySelector("#styleChoice4").checked = false;
+      document.querySelector("#styleChoice5").checked = false;
+      document.querySelector("#styleChoice6").checked = false;
+    }
+  } else if (style === "Yellow") {
+    let target = document.querySelectorAll(".color");
+    for (let i = 0; i < target.length; i++) {
+      target[i].classList.remove("blue");
+      target[i].classList.remove("pink");
+      target[i].classList.remove("normal");
+      target[i].classList.add("yellow");
+      target[i].classList.remove("green");
+      target[i].classList.remove("brown");
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice1").checked = true;
+      document.querySelector("#styleChoice3").checked = false;
+      document.querySelector("#styleChoice4").checked = false;
+      document.querySelector("#styleChoice5").checked = false;
+      document.querySelector("#styleChoice6").checked = false;
+    }
+  } else if (style === "Green") {
+    let target = document.querySelectorAll(".color");
+    for (let i = 0; i < target.length; i++) {
+      target[i].classList.remove("blue");
+      target[i].classList.remove("pink");
+      target[i].classList.remove("normal");
+      target[i].classList.remove("yellow");
+      target[i].classList.add("green");
+      target[i].classList.remove("brown");
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice3").checked = false;
+      document.querySelector("#styleChoice4").checked = true;
+      document.querySelector("#styleChoice5").checked = false;
+      document.querySelector("#styleChoice6").checked = false;
+    }
+  } else if (style === "Brown") {
+    let target = document.querySelectorAll(".color");
+    for (let i = 0; i < target.length; i++) {
+      target[i].classList.remove("blue");
+      target[i].classList.remove("pink");
+      target[i].classList.remove("normal");
+      target[i].classList.remove("yellow");
+      target[i].classList.remove("green");
+      target[i].classList.add("brown");
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice1").checked = false;
+      document.querySelector("#styleChoice3").checked = false;
+      document.querySelector("#styleChoice4").checked = false;
+      document.querySelector("#styleChoice5").checked = false;
+      document.querySelector("#styleChoice6").checked = true;
+    }
+  }
+}
+
+window.onload = function () {
+  applyStyle();
+  getUserStyleSetting();
+};
+
+//PAGE TRANSITIONS
+function pageTransition() {
+  var tl = gsap.timeline();
+
+  tl.to("ul.transition li", {
+    duration: 0.5,
+    scaleY: 1,
+    transformOrigin: "bottom left",
+    stagger: 0.2,
+  });
+  // tl.to("ul.transition li", {
+  //   duration: 0.5,
+  //   scaleY: 0,
+  //   transformOrigin: "bottom left",
+  //   stagger: 0.1,
+  //   delay: 0.1,
+  // });
+}
+
+function delay(n) {
+  n = n || 1300;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+barba.init({
+  sync: true,
+
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+
+        pageTransition();
+        await delay(1300);
+        done();
+      },
+
+      async enter(data) {
+        animateSlides();
+        applyStyle();
+        getUserStyleSetting();
+      },
+      async once(data) {
+        animateSlides();
+        onTick1();
+        onTick2();
+      },
+    },
+  ],
+});
